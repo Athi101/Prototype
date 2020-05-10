88e9768d07b8307ee4ac4b2262fd08428f9485ce
AbhilashG97 <abhilash.ag1997@gmail.com>
2020-05-10 19:02:12 +0530

    deleted stuff :tangerine:

--- app/src/main/java/com/mark32/prototype/MainActivity.java
+++ app/src/main/java/com/mark32/prototype/MainActivity.java
@@ -1,6 +1,11 @@
 package com.mark32.prototype;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,23 +15,55 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        checkBluetoothState();
+    }
 
+    private void checkBluetoothState() {
+        // Checks for the Bluetooth support and then makes sure it is turned on
+        // If it isn't turned on, request to turn it on
+        // List paired devices
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "\nBluetooth NOT supported. Aborting.", Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            if (mBluetoothAdapter.isEnabled()) {
+                Toast.makeText(this, "\nBluetooth is enabled...", Toast.LENGTH_LONG).show();
 
+                // Listing paired devices
+                Toast.makeText(this, "\n\nPaired Devices are:", Toast.LENGTH_LONG).show();
+
+                Log.v("BLUETOOTH", "\nPaired Devices are:");
+                Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+                for (BluetoothDevice device : devices) {
+                    Log.v("BLUETOOTH", "\n  Device: " + device.getName() + ", " + device);
+                }
+
+                mBluetoothAdapter.getBluetoothLeAdvertiser();
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            checkBluetoothState();
+        }
     }
 
 }
